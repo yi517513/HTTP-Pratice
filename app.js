43798ecdeca97e6b181773fd46406e95540e7702
YI <YI7513@gmail.com>
2024-05-01 17:27:50 +0800

    Response-1

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require("http");
 
 // request object, response object
-const server = http.createServer((req, res) => {}); // callback function with 2 parameter
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8 " });
+  res.write("歡迎來到我的網頁");
+  res.end();
+}); // callback function with 2 parameter
 
 server.listen(3000, () => {
   console.log("伺服器正在port3000上運行");
