14557ce5f9e4e98ab21963a00540f3aed494f155
YI <YI7513@gmail.com>
2024-05-01 18:30:34 +0800

    feat: 新增網頁路由功能 根據 req 的 URL，response 相應的內容 ("/", "/anotherPage", 以上皆非)

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 // request object, response object
-const server = http.createServer((req, res) => {}); // callback function with 2 parameter
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  if (req.url == "/") {
+    res.write("歡迎來到我的網頁");
+  } else if (req.url == "/anotherPage") {
+    res.write("這是另一個頁面");
+  } else {
+    res.write("不存在的頁面");
+  }
+}); // callback function with 2 parameter
 
 server.listen(3000, () => {
   console.log("伺服器正在port3000上運行");
